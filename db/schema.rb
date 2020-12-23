dd8665fd89b3a910d350724d6b392d8e4f9b8264
zhenyall <zhenya.lugovskoi95@gmail.com>
2020-12-23 23:25:34 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_144824) do
+ActiveRecord::Schema.define(version: 2020_12_23_212132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "add_author_ids", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
